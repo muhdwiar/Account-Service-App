e7b03aa168fafc187f12982abba631b2f90e15f2
muhdwiar <muhdwi45@gmail.com>
2022-08-26 00:27:58 +0700

    Update Read Tansfer History Feature

--- controllers/transaksi/transaksi_cont.go
+++ controllers/transaksi/transaksi_cont.go
@@ -129,7 +129,7 @@ func HistoryTP(db *sql.DB, user entities.User) ([]entities.Transaksi, error) {
 
 func GetTransfer_asSender(db *sql.DB, dataUser entities.User) ([]entities.Transaksi, error) {
 	action := "Transfer"
-	result_select, err_select := db.Query("SELECT tr.id, tr.nominal, tr.created_at, u.nama, u.no_telp FROM transaksi tr INNER JOIN user u ON tr.user_id_penerima = u.id WHERE tr.user_id = ? AND tr.action = ? ORDER BY tr.created_at;", &dataUser.ID, &action)
+	result_select, err_select := db.Query("SELECT tr.nominal, tr.created_at, u.nama, u.no_telp FROM transaksi tr INNER JOIN user u ON tr.user_id_penerima = u.id WHERE tr.user_id = ? AND tr.action = ? ORDER BY tr.created_at;", &dataUser.ID, &action)
 
 	if err_select != nil {
 		return nil, err_select
@@ -140,7 +140,7 @@ func GetTransfer_asSender(db *sql.DB, dataUser entities.User) ([]entities.Transa
 	for result_select.Next() {
 		var row_trans entities.Transaksi
 
-		errScan := result_select.Scan(&row_trans.ID, &row_trans.NOMINAL, &row_trans.CREATED_AT, &row_trans.USER.NAMA, &row_trans.USER.NO_TELP)
+		errScan := result_select.Scan(&row_trans.NOMINAL, &row_trans.CREATED_AT, &row_trans.USER.NAMA, &row_trans.USER.NO_TELP)
 
 		if errScan != nil {
 			return nil, errScan
@@ -156,7 +156,7 @@ func GetTransfer_asSender(db *sql.DB, dataUser entities.User) ([]entities.Transa
 
 func GetTransfer_asReceiver(db *sql.DB, dataUser entities.User) ([]entities.Transaksi, error) {
 
-	result_select, err_select := db.Query("SELECT tr.id, tr.nominal, tr.created_at, u.nama, u.no_telp FROM transaksi tr INNER JOIN user u ON tr.user_id = u.id WHERE tr.user_id_penerima = ? AND tr.action = ? ORDER BY tr.created_at;", &dataUser.ID, "Transfer")
+	result_select, err_select := db.Query("SELECT tr.nominal, tr.created_at, u.nama, u.no_telp FROM transaksi tr INNER JOIN user u ON tr.user_id = u.id WHERE tr.user_id_penerima = ? AND tr.action = ? ORDER BY tr.created_at;", &dataUser.ID, "Transfer")
 
 	if err_select != nil {
 		return nil, err_select
@@ -167,7 +167,7 @@ func GetTransfer_asReceiver(db *sql.DB, dataUser entities.User) ([]entities.Tran
 	for result_select.Next() {
 		var row_trans entities.Transaksi
 
-		errScan := result_select.Scan(&row_trans.ID, &row_trans.NOMINAL, &row_trans.CREATED_AT, &row_trans.USER.NAMA, &row_trans.USER.NO_TELP)
+		errScan := result_select.Scan(&row_trans.NOMINAL, &row_trans.CREATED_AT, &row_trans.USER.NAMA, &row_trans.USER.NO_TELP)
 
 		if errScan != nil {
 			return nil, errScan
